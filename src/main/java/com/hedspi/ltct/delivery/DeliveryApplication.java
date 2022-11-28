a24cad7663ac0479021b882e2338dabad0536e32
Rippo12 <nam0945702264@gmail.com>
2022-11-28 22:40:50 +0700

    test

--- src/main/java/com/hedspi/ltct/delivery/DeliveryApplication.java
+++ src/main/java/com/hedspi/ltct/delivery/DeliveryApplication.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+
+
 public class DeliveryApplication {
 
     public static void main(String[] args) {
