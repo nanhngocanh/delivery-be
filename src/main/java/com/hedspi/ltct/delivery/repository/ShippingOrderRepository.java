80fb5a8cfc3dfe70a43eda9441dd01ca52add571
DuskDuck <107291973+DuskDuck@users.noreply.github.com>
2022-11-30 15:16:47 +0700

    Delete src directory

--- src/main/java/com/hedspi/ltct/delivery/repository/ShippingOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hedspi.ltct.delivery.repository;
-
-import com.hedspi.ltct.delivery.model.ShippingOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShippingOrderRepository extends JpaRepository<ShippingOrder, Integer> {
-}
